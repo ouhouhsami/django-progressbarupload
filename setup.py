c8cad6cd9ecb7c1deccd8d8c9b0ce9919e88264c
ouhouhsami <samuel.goldszmidt@gmail.com>
2013-01-11 16:24:41 +0100

    up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-progressbarupload',
-    version = '0.1',
+    version = '0.1.1',
     packages = ['progressbarupload'],
     include_package_data = True,
     license = 'BSD License',
