f4cf9d96f9afae96f72b0d76c553d718b5872752
SebCorbin <seb.corbin@gmail.com>
2016-07-25 10:11:44 +0200

    Broken urls.py with python 3.5 and django 1.9

--- progressbarupload/urls.py
+++ progressbarupload/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-from views import upload_progress
+from progressbarupload.views import upload_progress
 
 urlpatterns = [
     url(r'^upload_progress$', upload_progress, name="upload_progress"),
