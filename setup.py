3dfa6cd466a188b50660d51a51518dc1e34dfb06
Sebastien Corbin <seb.corbin@gmail.com>
2016-11-04 23:03:14 +0100

    Wrong README extension

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
